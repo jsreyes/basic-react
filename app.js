c8f2aee22360c7dbe99a6fa39a29c241ea5b7fd7
Javier Sebastián Reyes Mogollón <jreyes@belatrixsf.com>
2020-02-14 14:16:16 -0500

    Create state for component

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function Servilleta() {
     return <h1>Soy una servilleta</h1>
 }
 
-// Declaración de un componente con Reac (Se cambian las funciones por las clases)
+// Declaración de un componente con React (Se cambian las funciones por las clases)
 class Tenedor extends React.Component {
     render() {
         return <h1>Esto es un tenedor</h1>
@@ -43,6 +43,10 @@ class Copa extends React.Component {
         this.state = {
             llena: false
         }
+
+        // Se bindean los metodos al componente
+        this.tomar = this.tomar.bind(this);
+        this.llenar = this.llenar.bind(this);
     }
 
     // Metodo que permite renderizar el componente
@@ -50,6 +54,21 @@ class Copa extends React.Component {
         // Dependiendo el estado del componente se visualiza una palabra o la otra
         return <h1>La copa { this.state ? 'tiene' : 'tenia' } { this.props.contenido }</h1>
     }
+
+    /*
+        Se crean dos funcione que cambiaran el estado de la app
+    */
+   tomar() {
+       this.state = {
+           llena: false
+       }
+   }
+
+   llenar() {
+       this.state = {
+           llena: true
+       }
+   }
 }
 
 <Copa contenido="vino"/>
